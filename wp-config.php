6609c150b352d77bb1de85499996b540bbf17a56
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2016-11-06 11:05:08 +0100

    data charge

--- wp-config.php
+++ wp-config.php
@@ -1,5 +1,5 @@
 <?php
-/** 
+/**
  * Configuración básica de WordPress.
  *
  * Este archivo contiene las siguientes configuraciones: ajustes de MySQL, prefijo de tablas,
@@ -16,19 +16,19 @@
 
 // ** Ajustes de MySQL. Solicita estos datos a tu proveedor de alojamiento web. ** //
 /** El nombre de tu base de datos de WordPress */
-define('DB_NAME', 'marinahurtado_web');
+define( 'DB_NAME', 'db4193755_marinhurtadal' );
 
 /** Tu nombre de usuario de MySQL */
-define('DB_USER', 'doggybra_admin');
+define( 'DB_USER', 'u4193755_mhaaul' );
 
 /** Tu contraseña de MySQL */
-define('DB_PASSWORD', 'hO1EBVyvD-');
+define( 'DB_PASSWORD', 'y2R,/N:Lm&Xcp;Sz' );
 
 /** Host de MySQL (es muy probable que no necesites cambiarlo) */
-define('DB_HOST', 'localhost');
+define( 'DB_HOST', 'localhost' );
 
 /** Codificación de caracteres para la base de datos. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** Cotejamiento de la base de datos. No lo modifiques si tienes dudas. */
 define('DB_COLLATE', '');
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY', 'LMVgU*r2?+<Iu6OwE,9gpISv>}c67IX/3o8A*SNIqFCT,XT2J|*)5H7M)E@4Y(-J');
-define('SECURE_AUTH_KEY', 'mD^%}wZ<(dUV*@aUCsdJ?`l5&wx%Y~oIcAG]bU+Mqv9!]7*+X^qnsst9NK:ld3iV');
-define('LOGGED_IN_KEY', 'IMERUE2U9`NRP,5BqN!FvSD@KuX#?V +l#c*1^io}}*V)XON M+QYTeA5_I:A0Il');
-define('NONCE_KEY', 'W#/92#7zo_<mribCi[:]9QJbW#AlS@y.W0OTV*}OqG?|!_uc+52Fcq 1_u#?l9Dr');
-define('AUTH_SALT', '!$mrC]1_qsenJ1sUZm?sVndG1l<t9;cgqNP=}WlD=zadRvQK;v])Wa,Y1yoA GS[');
-define('SECURE_AUTH_SALT', '@Y+V<&:_*5uqWWjj{j =4K#mj uiG%5tr~gb*%tcyzyODUg1+fMs6tDj;LA;!F2-');
-define('LOGGED_IN_SALT', 'F/+l:DOIt>=Npsb*81jNnl%lEod_u S4^LA4CDO-juVe0Sd6<fU[*>+-[XqMVV`4');
-define('NONCE_SALT', '9~]_eh2Na/cFrDRU8g44!-(sU~GW;+Hs!Wvaw;?#xA,@w_fMs}nI0-7 c:VbbSk3');
+define('AUTH_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('SECURE_AUTH_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('LOGGED_IN_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('NONCE_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('AUTH_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('SECURE_AUTH_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('LOGGED_IN_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('NONCE_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
 
 /**#@-*/
 
@@ -59,7 +59,7 @@ define('NONCE_SALT', '9~]_eh2Na/cFrDRU8g44!-(sU~GW;+Hs!Wvaw;?#xA,@w_fMs}nI0-7 c:
  * Cambia el prefijo si deseas instalar multiples blogs en una sola base de datos.
  * Emplea solo números, letras y guión bajo.
  */
-$table_prefix  = 'wp_';
+$table_prefix = 'wpp_';
 
 
 /**
